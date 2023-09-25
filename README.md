86f3de66122e6b4f8e6fbf5fb8b0aa54870b1eff
Patrik Muhammad Majeid <991294627p@gmail.com>
2023-09-25 11:06:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-final
+ estudos de html
---
+++ script/script.js
@@ -0,0 +1,115 @@
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.nav').addClass("sticky");
+    } else {
+      $('.nav').removeClass("sticky");
+    }
+  });
+});
+
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
+  });
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+  });
+
+  var typed = new Typed(".typing", {
+      strings:["DBA", "Desenvolvedor", "Designer", "O Milhor"],
+      typeSpeed:100,
+      backSpeed:600,
+      loop:true
+  });
+  var typed = new Typed(".typing-2", {
+      strings:["DBA", "Desenvolvedor", "Designer", "O Milhor"],
+      typeSpeed:100,
+      backSpeed:60,
+      loop:true
+  });
+
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+  $('.carousel').owlCarousel({
+      margin:20,
+      loop:true,
+      autoplayTimeOut:2000,
+      autoplayHoverPauser:true,
+      responsive:{
+          0:{
+              items:1,
+              nav:false
+          },
+          600:{
+              items:2,
+              nav:false
+          },
+          1000:{
+              items:3,
+              nav:false
+          }
+      }
+  });
+});
+
+function exibir(){
+  alert('Email Enviado com Sucesso!!')
+;
+}
